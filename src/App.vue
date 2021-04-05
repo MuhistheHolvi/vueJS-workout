fcf9203f7933a7b50d7834f872755910c0f9d93a
Mohammed Salman <mohammed@holvi.com>
2021-04-05 10:40:18 +0300

    Toasting machine v1

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <template>
   <div id="app">
-      <ul>
-        <li v-for="(sound, animal) in animals" :key="animal"> Animal {{animal}} says hello world as: {{sound}}.</li>
-      </ul>    
+    <button v-on:click="toast">Start toasting</button>
+    <ul v-show="showCountDown">
+      <p>Toasting</p>
+      <li v-for="number in countDown" :key="number">{{ number }}</li>
+    </ul>
+    <p v-show="toastingDone">Toasting done, enjoy your toast</p>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-    data() {
-      return {
-        animals: {
-          dog: "HowHow",
-          cat: "MeowMeow",
-          cow: "MooMoo",
+export default {
+  name: "App",
+  data() {
+    return {
+      countDown: 0,
+      toastingDone: false,
+      showCountDown: false,
+    };
+  },
+  methods: {
+    toast() {
+      setInterval(() => {
+        const isToast = isBreadToast(this.countDown);
+        if (isToast) {
+          this.toastingDone = true;
+        } else {
+          this.countDown++;
+          this.showCountDown = true;
         }
-      }
-    }
+      }, 1000);
+    },
+  },
+};
+function isBreadToast(counter) {
+  if (counter == 10) {
+    return true;
+  } else {
+    counter++;
+    return false;
   }
+}
 </script>
 
 <style>
